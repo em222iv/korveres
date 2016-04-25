729e3947fb0b9b6b0e8bfa08390dca796e8300ce
david G <dg222cs@student.lnu.se>
2016-04-25 16:09:24 +0200

    Environment setup of TodoMVC applications

---
+++ vue/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: "./js/app.js",
+  output: {
+    path: "./build",
+    publicPath: "/build/",
+    filename: "build.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.styl$/, loader: "style!css!stylus" },
+      { test: /\.html$/, loader: "html" }
+    ]
+  }
+}
